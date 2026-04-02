8d5dcdb9d0b77bb83e1210277e6d27c1c51fcffa
R Abiyyu Ardi Lian P <86779662+Abiyyuardi213@users.noreply.github.com>
2026-04-02 13:42:00 +0700

    fix claoidflare

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Cloudflare Turnstile -->
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+
     <!-- PWA & Apple Mobile Web Support -->
     <meta name="theme-color" content="#001f3f">
     <meta name="mobile-web-app-capable" content="yes">
@@ -99,8 +103,8 @@
                         Password?</a>
                 </div>
 
-                <div class="flex items-center justify-center py-4">
-                    <div id="turnstile-widget"></div>
+                <div class="flex items-center justify-center py-2">
+                    <div class="cf-turnstile" data-sitekey="{{ config('services.turnstile.key') }}"></div>
                 </div>
 
                 <button type="submit"
@@ -159,25 +163,6 @@
             })
         </script>
     @endif
-
-    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit" async defer></script>
-    <script>
-        window.onloadTurnstileCallback = function() {
-            if (document.getElementById('turnstile-widget')) {
-                turnstile.render('#turnstile-widget', {
-                    sitekey: '0x4AAAAAAADn6_9q_A_A_A_A',
-                    theme: 'light',
-                    callback: function(token) {
-                        console.log("Turnstile success!");
-                    },
-                });
-            }
-        };
-    </script>
-    {{-- Tambahkan parameter onload ke script Turnstile --}}
-    <script>
-        document.querySelector('script[src*="turnstile/v0/api.js"]').src += "&onload=onloadTurnstileCallback";
-    </script>
 </body>
 
 </html>
